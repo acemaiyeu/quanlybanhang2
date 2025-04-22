e3e9f648e0e2bc5249d6df04f5c9e7de782a8538
Huy FE <nthanhhuy82@gmail.com>
2025-04-22 18:55:24 +0700

    add params search variant

--- app/ModelQuery/VariantModel.php
+++ app/ModelQuery/VariantModel.php
@@ -15,6 +15,31 @@ class VariantModel
                 $query->where('name', 'like', '%' . $request['product_name'] . '%');
             });
         }
+        if (!empty($request['product_code'])) {
+            $query->whereHas('product', function ($query) use ($request) {
+                $query->where('code', 'like', '%' . $request['product_code'] . '%');
+            });
+        }
+        if (!empty($request['category_name'])) {
+            $query->whereHas('product', function ($query) use ($request) {
+                $query->whereHas('category', function ($query) use ($request) {
+                    $query->where('name', 'like', '%' . $request['category_name'] . '%');
+                });
+            });
+        }
+        if (!empty($request['category_code'])) {
+            $query->whereHas('product', function ($query) use ($request) {
+                $query->whereHas('category', function ($query) use ($request) {
+                    $query->where('code', 'like', '%' . $request['category_code'] . '%');
+                });
+            });
+        }
+
+        if (!empty($request['sort'])) {
+            foreach ($request['sort'] as $key => $value) {
+                $query->orderBy($key, $value);
+            }
+        }
         $query->with('product.unit');
         $limit = $request['limit'] ?? 10;
         return $limit == 1 ? $query->first() : $query->paginate($limit);
